84ce4224a47ce4350b8819d3c35bbfd45d5b0f4f
Lee Saville <l.w.saville@gmail.com>
2016-11-28 21:11:47 -0700

    Add test for /invoices/:id

--- spec/requests/api/v1/invoices_request_spec.rb
+++ spec/requests/api/v1/invoices_request_spec.rb
@@ -12,4 +12,14 @@ describe 'Invoices endpoints' do
       expect(items.count).to eq(3)
     end
   end
+
+  context 'GET /invoices/:id' do
+    it 'returns an invoice' do
+      item = create(:invoice, status: 'suuuper')
+      get "/api/v1/invoices/#{item.id}"
+
+      expect(response).to be_success
+      expect(item.status).to eq('suuuper')
+    end
+  end
 end
