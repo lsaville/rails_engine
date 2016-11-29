bf588c736a13598aadec00e00dc6051bcf850e24
Lee Saville <l.w.saville@gmail.com>
2016-11-29 16:53:48 -0700

    Rewrite the routes for merchants and items to not use resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,22 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       namespace :items do
-        get 'find', to: 'search#show'
+        get '', to: 'items#index'
+        get '/find', to: 'search#show'
+        get '/:id', to: 'items#show'
       end
-      resources :merchants, only: [:index, :show]
+
+      namespace :merchants do
+        get '', to: 'merchants#index'
+        get '/:id/customers_with_pending_invoices', to: 'customers_pending_invoices#index'
+        get '/:id', to: 'merchants#show'
+      end
+
+      #resources :merchants, only: [:index, :show]
       resources :transactions, only: [:index, :show]
       resources :customers, only: [:index, :show]
       resources :invoices, only: [:index, :show]
-      resources :items, only: [:index, :show]
+      #resources :items, only: [:index, :show]
       resources :invoice_items, only: [:index, :show]          
     end
   end
