401f7bee8c9bcb94262ffcbb270c8db7c0af2ea4
Lee Saville <l.w.saville@gmail.com>
2016-11-29 13:05:52 -0700

    Add nested random route to random controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       namespace :items do
         get 'find', to: 'search#show'
+        get 'find_all', to: 'search#index'
+        get 'random', to: 'random#show'
       end
       resources :merchants, only: [:index, :show]
       resources :transactions, only: [:index, :show]
