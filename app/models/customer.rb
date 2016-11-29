a720697d45f7ed997c0c7ba4049c1dcb0bee11ce
Lee Saville <l.w.saville@gmail.com>
2016-11-28 20:45:17 -0700

    Fix errant comma typo

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ApplicationRecord
   has_many :invoices
   has_many :merchants, through: :invoices
-  validates :first_name, , :last_name, presence: true
+  validates :first_name, :last_name, presence: true
 end
