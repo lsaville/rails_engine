ecb77279c5ed2d20b381f634708bde554d0801a2
Lee Saville <l.w.saville@gmail.com>
2016-11-29 13:06:31 -0700

    Add testing for items/random

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -46,5 +46,43 @@ describe 'Items endpoints' do
       expect(response).to be_success
       expect(item['id']).to eq(@item.id)
     end
+    
+    it 'returns a single item case insensitive' do
+      create(:item, name: 'CRAZY')
+      get '/api/v1/items/find?name=crazy'
+
+      item = JSON.parse(response.body)
+
+      expect(item['name']).to eq('CRAZY')
+    end
+  end
+
+  context 'GET /items/find_all?parameters' do
+    it 'returns a list of items based on the parameters' do
+      create_list(:item, 3, name: 'bruce')
+      get "/api/v1/items/find_all?name=bruce"
+
+      items = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(items.count).to eq(3)
+    end
+  end
+
+  context 'GET /items/random' do
+    it 'returns an item randomly' do
+      create_list(:item, 10)
+      get '/api/v1/items/random'
+
+      item1 = JSON.parse(response.body)
+
+      expect(response).to be_success
+      
+      get '/api/v1/items/random'
+
+      item2 = JSON.parse(response.body)
+
+      expect(item1['id']).to_not eq(item2['id'])
+    end
   end
 end
