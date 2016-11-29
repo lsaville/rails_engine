ab5787a74a857162a039861e42fc749b677d2450
Lee Saville <l.w.saville@gmail.com>
2016-11-28 22:01:46 -0700

    Fix typo

--- spec/requests/api/v1/invoices_request_spec.rb
+++ spec/requests/api/v1/invoices_request_spec.rb
@@ -6,20 +6,20 @@ describe 'Invoices endpoints' do
       create_list(:invoice, 3)
       get '/api/v1/invoices'
 
-      items = JSON.parse(response.body)
+      invoices = JSON.parse(response.body)
 	      
       expect(response).to be_success
-      expect(items.count).to eq(3)
+      expect(invoices.count).to eq(3)
     end
   end
 
   context 'GET /invoices/:id' do
     it 'returns an invoice' do
-      item = create(:invoice, status: 'suuuper')
-      get "/api/v1/invoices/#{item.id}"
+      invoice = create(:invoice, status: 'suuuper')
+      get "/api/v1/invoices/#{invoice.id}"
 
       expect(response).to be_success
-      expect(item.status).to eq('suuuper')
+      expect(invoice.status).to eq('suuuper')
     end
   end
 end
