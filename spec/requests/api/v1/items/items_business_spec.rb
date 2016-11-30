8565151301cb28805c363236bb2ed4f24946b953
Lee Saville <l.w.saville@gmail.com>
2016-11-30 07:08:40 -0700

    Add skeleton of items business endpoint spec

---
+++ spec/requests/api/v1/items/items_business_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe 'Items business endpoints' do
+  context 'GET /most_revenue?quantity=x' do
+    it 'returns top x items ranked by total revenue generated' do
+
+    end
+  end
+end
