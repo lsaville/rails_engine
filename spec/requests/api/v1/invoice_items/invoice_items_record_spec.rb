479789bea6b7f08b114582bfb61b66a7fe9d24d7
Lee Saville <l.w.saville@gmail.com>
2016-11-29 20:37:47 -0700

    Add test for find_all

--- spec/requests/api/v1/invoice_items/invoice_items_record_spec.rb
+++ spec/requests/api/v1/invoice_items/invoice_items_record_spec.rb
@@ -24,4 +24,32 @@ describe 'Invoice_tems endpoints' do
       expect(response_invoice_item['quantity']).to eq(80)
     end
   end
+
+  context 'GET /invoice_items/find?params' do
+    it 'retruns an invoice_item based on passed params' do
+      create(:invoice_item, unit_price: 12345678)
+      create(:invoice_item, unit_price: 0)
+
+      get '/api/v1/invoice_items/find?unit_price=0'
+
+      invoice_item = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(invoice_item['unit_price']).to eq(0)
+    end
+  end
+
+  context 'GET /invoice_items/find_all?params' do
+    it 'returns all invoice_items based on params' do
+      create_list(:invoice_item, 3, unit_price: 3)
+      create(:invoice_item)
+
+      get '/api/v1/invoice_items/find_all?unit_price=3'
+
+      invoices = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(invoices.count).to eq(3)
+    end
+  end
 end
