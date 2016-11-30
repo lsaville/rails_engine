0b52916158f3b35e2918372406dac5a92f4bb6b0
Lee Saville <l.w.saville@gmail.com>
2016-11-29 22:29:31 -0700

    Add merchants/:id/items route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
         get 'find_all', to: 'search#index'
         get 'random', to: 'random#show'
         get 'most_items', to: 'most_items#index'
+        get ':id/items', to: 'items#index'
       end
 
       namespace :customers do
