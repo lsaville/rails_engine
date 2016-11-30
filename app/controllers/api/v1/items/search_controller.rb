c37e187cc9015f6abdf5282c58e54caeb0dc08cd
Lee Saville <l.w.saville@gmail.com>
2016-11-29 20:39:57 -0700

    Change find_params to better reflect function

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,15 +1,15 @@
 class Api::V1::Items::SearchController < ApplicationController
   def show
-    render json: Item.find_by(find_params)
+    render json: Item.find_by(items_search_criteria)
   end
 
   def index
-    render json: Item.where(find_params)
+    render json: Item.where(items_search_criteria)
   end
 
   private
 
-  def find_params
+  def items_search_criteria
     params.permit(:name, :id, :created_at, :updated_at)
   end
 end
