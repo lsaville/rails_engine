0ba27eef7105e649139c2c54e04f5bab3597da07
Dave Junta <djunta@gmail.com>
2016-11-29 17:37:14 -0700

    again

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,5 +2,6 @@ class Merchant < ApplicationRecord
   has_many :invoices
   has_many :items
   has_many :customers, through: :invoices
+  has_many :invoice_items, through: :invoices
   validates :name, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
         get 'find', to: 'search#show'
         get 'find_all', to: 'search#index'
         get 'random', to: 'random#show'
-        get 'most_items' to: 'most_items#index'
+        get 'most_items', to: 'most_items#index'
       end
       namespace :customers do
         get 'find', to: 'search#show'
