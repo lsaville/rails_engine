5c407efc2b67519e8e2bb334171197a0815080b9
Dave Junta <djunta@gmail.com>
2016-11-29 22:18:15 -0700

    add merchant_most_items method and controller

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,4 +4,24 @@ class Merchant < ApplicationRecord
   has_many :customers, through: :invoices
   has_many :invoice_items, through: :invoices
   validates :name, presence: true
+
+  def self.merchants_most_items(quantity)
+    joins(:invoice_items).
+    joins("INNER JOIN transactions ON transactions.invoice_id = invoice_items.invoice_id").
+    where("transactions.result = 'success'").
+    group('merchants.id').
+    order("sum(invoice_items.quantity) desc").
+    limit(quantity)
+  end
+
+  def self.merchants_most_revenue(quantity)
+    joins(:invoice_items).
+    joins("INNER JOIN transactions ON transactions.invoice_id = invoice_items.invoice_id").
+    where("transactions.result = 'success'").
+    group('merchants.id').
+    order("sum(invoice_items.quantity * invoice_items.unit_price) desc").
+    limit(quantity)
+  end
+
+
 end
