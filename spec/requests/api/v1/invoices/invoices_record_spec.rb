2286d9206568f5fe3f0874bc0b3942bc2e511f06
Lee Saville <l.w.saville@gmail.com>
2016-11-29 19:54:00 -0700

    Add test for /invoices/find?params

--- spec/requests/api/v1/invoices/invoices_record_spec.rb
+++ spec/requests/api/v1/invoices/invoices_record_spec.rb
@@ -24,4 +24,18 @@ describe 'Invoices endpoints' do
       expect(response_invoice['status']).to eq('suuuper')
     end
   end
+
+  context 'GET /invoices/find?parameters' do
+    it 'returns an invoice based on the string query params' do
+      create_list(:invoice, 3)
+      create(:invoice, status: 'fluffy')
+
+      get '/api/v1/invoices/find?status=fluffy'
+
+      invoice = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(invoice['status']).to eq('fluffy')
+    end
+  end
 end
