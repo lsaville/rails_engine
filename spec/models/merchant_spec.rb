1320a0dc0122abb66b4477151c658bd2059d82b5
Lee Saville <l.w.saville@gmail.com>
2016-12-02 08:40:26 -0700

    Add test for merchants_most_items

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -33,4 +33,24 @@ describe Merchant, type: :model do
       expect(merchant).to_not be_valid
     end
   end
+
+  context 'abilities' do
+    it 'can return top x merchants by ranked by total number of items sold' do
+      merchant1 = create(:merchant, name: 'Alejandra')
+      merchant2 = create(:merchant, name: 'Billy')
+      customer = create(:customer)
+      invoice1 = create(:invoice, merchant: merchant1, customer: customer)
+      invoice2 = create(:invoice, merchant: merchant1, customer: customer)
+      invoice3 = create(:invoice, merchant: merchant2, customer: customer)
+      invoice_item1 = create(:invoice_item, invoice: invoice1, quantity: 10)
+      invoice_item2 = create(:invoice_item, invoice: invoice2, quantity: 1)
+      invoice_item2 = create(:invoice_item, invoice: invoice3, quantity: 1)
+      create(:transaction, result: 'success', invoice: invoice1)
+      create(:transaction, result: 'success', invoice: invoice2)
+
+      result = Merchant.merchants_most_items(2)
+
+      expect(result.first['name']).to eq(merchant1.name)
+    end
+  end
 end
