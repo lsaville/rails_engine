108bc3e387120986802eef5286eac4b5a4319c16
Lee Saville <l.w.saville@gmail.com>
2016-12-01 19:08:33 -0700

    Add call to customers with pending invoices

--- app/controllers/api/v1/merchants/customers_pending_invoices_controller.rb
+++ app/controllers/api/v1/merchants/customers_pending_invoices_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::Merchants::CustomersPendingInvoicesController < ApplicationController
   def index
+    render json: Merchant.find(params[:id]).customers_with_pending_invoices
   end
-
 end
