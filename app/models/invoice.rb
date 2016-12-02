234aba3d97b8630ecb91ff3fea5fdd66857f3ff3
Lee Saville <l.w.saville@gmail.com>
2016-12-02 07:33:07 -0700

    Delete unused scope

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -5,9 +5,4 @@ class Invoice < ApplicationRecord
   has_many :invoice_items
   has_many :items, through: :invoice_items
   validates :status, presence: true
-
-  scope :by_date, -> (date) { where("invoices.created_at >= ? AND invoices.created_at <= ?",
-                                     date.beginning_of_day,
-                                     date.end_of_day)
-                             } 
 end
